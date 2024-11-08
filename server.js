77987573a82cb1838c37902a335e3bac54400814
unknown <sekerbekovbeksultan2@gmail.com>
2024-11-08 12:04:17 +0600

    last

--- server.js
+++ server.js
@@ -4,16 +4,25 @@ const server = new WebSocket.Server({ port: 8080 });
 
 server.on('connection', socket => {
     console.log('Новое соединение установлено');
-    socket.on('message', message => {
-        console.log(`Получено сообщение: ${message}`); // Логируем сообщение
 
-        // Отправляем команды всем подключенным клиентам как строки
+    socket.on('message', message => {
+        console.log(`Получено сообщение: ${message}`); // Логируем полученное сообщение
+
+        // Отправляем команду всем подключенным клиентам
         const messageStr = message.toString();  // Преобразуем сообщение в строку
         server.clients.forEach(client => {
             if (client.readyState === WebSocket.OPEN) {
                 client.send(messageStr);  // Отправляем как строку
             }
         });
+    });
+
+    socket.on('close', () => {
+        console.log('Соединение закрыто');
+    });
+
+    socket.on('error', (err) => {
+        console.error('Ошибка WebSocket на сервере:', err);
     });
 });
 
